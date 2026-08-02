28435a43364c7918866bc7b8be24abec8afd99ef
chrishor29 <chrishor29@gmail.com>
2026-08-02 22:49:03 +0200

    make some changes

--- szemeszter.bat
+++ szemeszter.bat
@@ -23,25 +23,25 @@ xcopy /y %source%\REZIDENS\labor.html %destination%\REZIDENS\ /d
 xcopy /y %source%\English\english.html %destination%\English\ /d
 
 REM 6.év
-xcopy /y %source%\VI\Allamvizsga\teszt.html %destination%\Allamvizsga\ /d
-xcopy /y %source%\VI\Allamvizsga\zarov.html %destination%\Allamvizsga\ /d
-xcopy /y %source%\VI\Belgyogy\belgyogyAV.html %destination%\Belgyogy\ /d
-xcopy /y %source%\VI\Belgyogy\belgyogy.html %destination%\Belgyogy\ /d
-xcopy /y %source%\VI\Neurologia\neuroAV.html %destination%\Neurologia\ /d
-xcopy /y %source%\VI\Neurologia\neurologia.html %destination%\Neurologia\ /d
-xcopy /y %source%\VI\Gyerekgyogy\gyermekAV.html %destination%\Gyerekgyogy\ /d
-xcopy /y %source%\VI\Gyerekgyogy\gyermek.html %destination%\Gyerekgyogy\ /d
-xcopy /y %source%\VI\Pszichiatria\pszichiAV.html %destination%\Pszichiatria\ /d
-xcopy /y %source%\VI\Pszichiatria\pszichi.html %destination%\Pszichiatria\ /d
-xcopy /y %source%\VI\Szuleszet\szuleszetAV.html %destination%\Szuleszet\ /d
-xcopy /y %source%\VI\Szuleszet\szuleszet.html %destination%\Szuleszet\ /d
-xcopy /y %source%\VI\Sebeszet\sebeszetAV.html %destination%\Sebeszet\ /d
-xcopy /y %source%\VI\Sebeszet\sebeszet.html %destination%\Sebeszet\ /d
+xcopy /y %source%\VI\Allamvizsga\teszt.html %destination%\VI\Allamvizsga\ /d
+xcopy /y %source%\VI\Allamvizsga\zarov.html %destination%\VI\Allamvizsga\ /d
+xcopy /y %source%\VI\Belgyogy\belgyogyAV.html %destination%\VI\Belgyogy\ /d
+xcopy /y %source%\VI\Belgyogy\belgyogy.html %destination%\VI\Belgyogy\ /d
+xcopy /y %source%\VI\Neurologia\neuroAV.html %destination%\VI\Neurologia\ /d
+xcopy /y %source%\VI\Neurologia\neurologia.html %destination%\VI\Neurologia\ /d
+xcopy /y %source%\VI\Gyerekgyogy\gyermekAV.html %destination%\VI\Gyerekgyogy\ /d
+xcopy /y %source%\VI\Gyerekgyogy\gyermek.html %destination%\VI\Gyerekgyogy\ /d
+xcopy /y %source%\VI\Pszichiatria\pszichiAV.html %destination%\VI\Pszichiatria\ /d
+xcopy /y %source%\VI\Pszichiatria\pszichi.html %destination%\VI\Pszichiatria\ /d
+xcopy /y %source%\VI\Szuleszet\szuleszetAV.html %destination%\VI\Szuleszet\ /d
+xcopy /y %source%\VI\Szuleszet\szuleszet.html %destination%\VI\Szuleszet\ /d
+xcopy /y %source%\VI\Sebeszet\sebeszetAV.html %destination%\VI\Sebeszet\ /d
+xcopy /y %source%\VI\Sebeszet\sebeszet.html %destination%\VI\Sebeszet\ /d
 
 REM 5.év
-xcopy /y %source%\VI\Belgyogy\hiirlm.html %destination%\Belgyogy\ /d
-xcopy /y %source%\VI\Neurologia\neuroteszt.html %destination%\Neurologia\ /d
-xcopy /y %source%\VI\Szuleszet\nogyogy.html %destination%\Szuleszet\ /d
+xcopy /y %source%\VI\Belgyogy\hiirlm.html %destination%\VI\Belgyogy\ /d
+xcopy /y %source%\VI\Neurologia\neuroteszt.html %destination%\VI\Neurologia\ /d
+xcopy /y %source%\VI\Szuleszet\nogyogy.html %destination%\VI\Szuleszet\ /d
 xcopy /y %source%\done\Aneszt\aneszt.html %destination%\done\Aneszt\ /d
 xcopy /y %source%\done\Bioetika\bioetika.html %destination%\done\Bioetika\ /d
 xcopy /y %source%\done\IgOr\igor.html %destination%\done\IgOr\ /d
@@ -53,7 +53,7 @@ xcopy /y %source%\done\Transzfuziologia\trafo.html %destination%\done\Transzfuzi
 xcopy /y %source%\done\Urologia\urologia.html %destination%\done\Urologia\ /d
 
 REM 4.év
-xcopy /y %source%\VI\Belgyogy\nephro.html %destination%\Belgyogy\ /d
+xcopy /y %source%\VI\Belgyogy\nephro.html %destination%\VI\Belgyogy\ /d
 xcopy /y %source%\done\Borgyogy\borgyogy.html %destination%\done\Borgyogy\ /d
 xcopy /y %source%\done\Onkologia\onko.html %destination%\done\Onkologia\ /d
 xcopy /y %source%\done\Ortopedia\ortop.html %destination%\done\Ortopedia\ /d
@@ -69,8 +69,8 @@ xcopy /y %source%\done\Kardio\favicon.bmp %destination%\done\Kardio\ /d
 xcopy /y %source%\done\Kardio\kardio.html %destination%\done\Kardio\ /d
 
 REM 3.év
-xcopy /y %source%\VI\Belgyogy\endokrin.html %destination%\Belgyogy\ /d
-xcopy /y %source%\VI\Belgyogy\proped.html %destination%\Belgyogy\ /d
+xcopy /y %source%\VI\Belgyogy\endokrin.html %destination%\VI\Belgyogy\ /d
+xcopy /y %source%\VI\Belgyogy\proped.html %destination%\VI\Belgyogy\ /d
 xcopy /y %source%\done\Genetika\favicon.bmp %destination%\done\Genetika\ /d
 xcopy /y %source%\done\Genetika\genetika.html %destination%\done\Genetika\ /d
 xcopy /y %source%\done\Immunologia\favicon.bmp %destination%\done\Immunologia\ /d
